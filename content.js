05e5ae0685e869e42d8f766bd77bc3160066c988
Abdelrahman Ghanem <a.ghanem2244@gmail.com>
2020-10-26 09:33:31 +0000

    add code in files types: .php,.js remove code in files types: .php,.js

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
 console.log("wdkgo")
 console.log("dfogc")
 console.log("pbnsr")
-console.log("qeqxs")
+console.log("nyxwi")
--- content.php
+++ content.php
@@ -4,5 +4,5 @@
         echo "iyuik";
         echo "dudnk";
         echo "umnhc";
-        echo "tlvhw";
+        echo "xduma";
 ?>
